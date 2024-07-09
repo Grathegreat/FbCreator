ebf4cb3beaad2b0edd10d52f39f5633a28e5c2ee
Grathegreat <onlyonehacker999@gmail.com>
2024-07-09 16:12:22 +0800

    Update create.py

--- create.py
+++ create.py
@@ -1,12 +1,27 @@
-print(f"""
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓           
-❖ › Github :- @grathegreat 
-❖ › By      :- Mot Mot Oyamat
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛                """)
-print('\x1b[38;5;208m⇼'*60)
-print('\x1b[38;5;22m•'*60)
-print('\x1b[38;5;22m•'*60)
-print('\x1b[38;5;208m⇼'*60)
+# Import the necessary modules
+import os
+
+# Function to clear the terminal screen in Termux
+def clear_screen():
+    os.system('clear')  # Termux clear command
+
+# Clear the screen
+clear_screen()
+
+# Your ASCII art with color formatting using ANSI escape codes
+banner = r"""
+\x1b[38;5;196m██████╗██████╗ ███████╗ █████╗ ████████╗███████╗
+██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝
+██║     ██████╔╝█████╗  ███████║   ██║   █████╗  
+██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝  
+╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗
+ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
+"""
+
+# Print the banner with lolcat coloring
+os.system(f'echo "{banner}" | lolcat')
+
+# Rest of your code continues here
 import requests
 import random
 import string
@@ -14,10 +29,12 @@ import json
 import hashlib
 from faker import Faker
 
+# Function to generate a random string
 def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(length))
 
+# Function to get available mail domains
 def get_mail_domains():
     url = "https://api.mail.tm/domains"
     try:
@@ -31,6 +48,7 @@ def get_mail_domains():
         print(f'[×] Error : {e}')
         return None
 
+# Function to create a mail.tm account
 def create_mail_tm_account():
     fake = Faker()
     mail_domains = get_mail_domains()
@@ -43,7 +61,7 @@ def create_mail_tm_account():
         last_name = fake.last_name()
         url = "https://api.mail.tm/accounts"
         headers = {"Content-Type": "application/json"}
-        data = {"address": f"{username}@{domain}", "password":password}       
+        data = {"address": f"{username}@{domain}", "password": password}       
         try:
             response = requests.post(url, headers=headers, json=data)
             if response.status_code == 201:
@@ -56,44 +74,72 @@ def create_mail_tm_account():
             print(f'[×] Error : {e}')
             return None, None, None, None, None
 
-# The following function is commented out to avoid any interaction with Facebook API.
-# def register_facebook_account(email, password, first_name, last_name, birthday):
-    # api_key = '882a8490361da98702bf97a021ddc14d'
-    # secret = '62f8ce9f74b12f84c123cc23437a4a32'
-    # gender = random.choice(['M', 'F'])
-    # req = {'api_key': api_key,'attempt_login': True,'birthday': birthday.strftime('%Y-%m-%d'),'client_country_code': 'EN','fb_api_caller_class': 'com.facebook.registration.protocol.RegisterAccountMethod','fb_api_req_friendly_name': 'registerAccount','firstname': first_name,'format': 'json','gender': gender,'lastname': last_name,'email': email,'locale': 'en_US','method': 'user.register','password': password,'reg_instance': generate_random_string(32),'return_multiple_errors': True}
-     sorted_req = sorted(req.items(), key=lambda x: x[0])
-     sig = ''.join(f'{k}={v}' for k, v in sorted_req)
-     ensig = hashlib.md5((sig + secret).encode()).hexdigest()
-     req['sig'] = ensig
-     api_url = 'https://b-api.facebook.com/method/user.register'
-     reg = _call(api_url, req)
-     id = reg['new_user_id']
-     token = reg['session_info']['access_token']
-     print(f'''
- \x1b[38;5;22m•▬▭▬▭▬▭▬|𓆩Mot𓆪|▬▭▬▭▬▭▬•
- ﴾𝐕𝐈𝐏﴿ EMAIL : {email}
- ﴾𝐕𝐈𝐏﴿ ID : {id}
- ﴾𝐕𝐈𝐏﴿ PASSWORD : {password}
- ﴾𝐕𝐈𝐏﴿ NAME : {first_name} {last_name}
- ﴾𝐕𝐈𝐏﴿ BIRTHDAY : {birthday} 
- ﴾𝐕𝐈𝐏﴿GENDER : {gender}
- •▬▭▬▭▬▭▬|𓆩Mot𓆪|▬▭▬▭▬▭▬•
- ﴾𝐕𝐈𝐏﴿ Token : {token}
- •▬▭▬▭▬▭▬|𓆩Mot𓆪|▬▭▬▭▬▭▬•''')
- open('username.txt','a')
+# Function to register a Facebook account
+def register_facebook_account(email, password, first_name, last_name, birthday):
+    api_key = '882a8490361da98702bf97a021ddc14d'
+    secret = '62f8ce9f74b12f84c123cc23437a4a32'
+    gender = random.choice(['M', 'F'])
+    req = {
+        'api_key': api_key,
+        'attempt_login': True,
+        'birthday': birthday.strftime('%Y-%m-%d'),
+        'client_country_code': 'EN',
+        'fb_api_caller_class': 'com.facebook.registration.protocol.RegisterAccountMethod',
+        'fb_api_req_friendly_name': 'registerAccount',
+        'firstname': first_name,
+        'format': 'json',
+        'gender': gender,
+        'lastname': last_name,
+        'email': email,
+        'locale': 'en_US',
+        'method': 'user.register',
+        'password': password,
+        'reg_instance': generate_random_string(32),
+        'return_multiple_errors': True
+    }
+    sorted_req = sorted(req.items(), key=lambda x: x[0])
+    sig = ''.join(f'{k}={v}' for k, v in sorted_req)
+    ensig = hashlib.md5((sig + secret).encode()).hexdigest()
+    req['sig'] = ensig
+    api_url = 'https://b-api.facebook.com/method/user.register'
+    reg = _call(api_url, req)
+    id = reg['new_user_id']
+    token = reg['session_info']['access_token']
+    print(f'''
+\x1b[38;5;22m⋘▬▭▬▭▬▭▬﴾𓆩OK𓆪﴿▬▭▬▭▬▭▬⋙
+﴾𝐕𝐈𝐏﴿ EMAIL : {email}
+﴾𝐕𝐈𝐏﴿ ID : {id}
+﴾𝐕𝐈𝐏﴿ PASSWORD : {password}
+﴾𝐕𝐈𝐏﴿ NAME : {first_name} {last_name}
+﴾𝐕𝐈𝐏﴿ BIRTHDAY : {birthday} 
+﴾𝐕𝐈𝐏﴿GENDER : {gender}
+⋘▬▭▬▭▬▭▬﴾𓆩OK𓆪﴿▬▭▬▭▬▭▬⋙
+﴾𝐕𝐈𝐏﴿ Token : {token}
+⋘▬▭▬▭▬▭▬﴾𓆩OK𓆪﴿▬▭▬▭▬▭▬⋙''')
+    open('username.txt','a')
 
+# Function to make API call
 def _call(url, params, post=True):
-    headers = {'User-Agent': '[FBAN/FB4A;FBAV/35.0.0.48.273;FBDM/{density=1.33125,width=800,height=1205};FBLC/en_US;FBCR/;FBPN/com.facebook.katana;FBDV/Nexus 7;FBSV/4.1.1;FBBK/0;]'}
+    headers = {
+        'User-Agent': '[FBAN/FB4A;FBAV/35.0.0.48.273;FBDM/{density=1.33125,width=800,height=1205};FBLC/en_US;FBCR/;FBPN/com.facebook.katana;FBDV/Nexus 7;FBSV/4.1.1;FBBK/0;]'
+    }
     if post:
         response = requests.post(url, data=params, headers=headers)
     else:
         response = requests.get(url, params=params, headers=headers)
     return response.json()
 
-for i in range(int(input('[+] How Many Accounts You Want:  '))):
-    email, password, first_name, last_name, birthday = create_mail_tm_account()
-    if email and password and first_name and last_name and birthday:
-        print(f'Created email: {email} with password: {password} and name: {first_name} {last_name}, birthday: {birthday}')
+# Main program logic
+if __name__ == "__main__":
+    try:
+        # Ask user for number of accounts to create
+        num_accounts = int(input('[+] How Many Accounts You Want: '))
 
-print('\x1b[38;5;208m⇼'*60)
+        # Loop to create specified number of accounts
+        for i in range(num_accounts):
+            email, password, first_name, last_name, birthday = create_mail_tm_account()
+            if email and password and first_name and last_name and birthday:
+                register_facebook_account(email, password, first_name, last_name, birthday)
+
+    except ValueError:
+        print("Please enter a valid number.")
